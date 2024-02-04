02d6257dd2213903b7ef2a0bd308cf55f199787c
Berkay Sunay <87619769+aethrius0@users.noreply.github.com>
2024-02-04 18:07:41 +0300

    Update notdefteri.py
    Koddaki son hatalar giderilmiş ve son hali atılmıştır.

--- notdefteri.py
+++ notdefteri.py
@@ -9,14 +9,13 @@ class Pencere(QtWidgets.QWidget):
         
     def init_ui(self):
         
-        self.baslik_1=QtWidgets.QLabel(self)
-        self.baslik_2=QtWidgets.QLabel(self)
-        self.baslik_3=QtWidgets.QLabel(self)
-        self.baslik_4=QtWidgets.QLabel(self)
-        self.yazi_alani=QtWidgets.QTextEdit(self)
+        self.setWindowTitle("Not defteri")  #Arayüzün başlığı
+        
+        self.yazi_alani=QtWidgets.QTextEdit(self)   #Notları aldığımız ana kısım
         self.yazi_alani.setGeometry(10,10,350,400)
         
-        #kutucukları ayrı ayrı silme fonksiyonu ve radio butonların tanımları 
+        
+        #Kutucukları ayrı ayrı silme fonksiyonu ve radio butonların tanımları 
         self.sec1 = QtWidgets.QRadioButton("",self)
         self.sec2 = QtWidgets.QRadioButton("",self)
         self.sec3 = QtWidgets.QRadioButton("",self)
@@ -26,24 +25,21 @@ class Pencere(QtWidgets.QWidget):
         self.buton_temizle = QtWidgets.QPushButton("Temizle",self)
         self.buton_kaydet=QtWidgets.QPushButton("Kaydet",self)
         
-        self.buton_temizle.move(500,420)
+        self.buton_temizle.move(500,420)    #Buton ve radio butonlarının yerleri
         self.buton_ac.move(400,420)
         self.sec1.move(380,45)
         self.sec2.move(380,150)
         self.sec3.move(380,260)
         self.sec4.move(380,370)
         self.buton_kaydet.move(150,420)
-        self.baslik_1.move(400,10)
-        self.baslik_2.move(400,115)
-        self.baslik_3.move(400,220)
-        self.baslik_4.move(400,325)
         
-        self.buton_temizle.clicked.connect(lambda : self.temizle_clicked())
+        
+        self.buton_temizle.clicked.connect(lambda : self.temizle_clicked())    #Butonların bağlanma noktaları
         self.buton_ac.clicked.connect(self.ac_clicked)
         self.buton_kaydet.clicked.connect(self.kaydet_clicked)
         
         
-        #Sağ tarafta kaydedilen notların olduğu kısım
+        #Sağ tarafta kaydedilen notların olduğu kısım-----
         self.kayitli_kutucuk=QtWidgets.QTextEdit(self)
         self.kayitli_kutucuk.setGeometry(400,30,200,60)
         
@@ -55,18 +51,33 @@ class Pencere(QtWidgets.QWidget):
         
         self.kayitli_kutucuk_4=QtWidgets.QTextEdit(self)
         self.kayitli_kutucuk_4.setGeometry(400,345,200,60)
+        
+        self.baslik_1=QtWidgets.QLabel(self)  #Sağdaki kayıtlı notların başlıklarının yazılıp yerlerinin belirlendiği kısım
+        self.baslik_2=QtWidgets.QLabel(self)
+        self.baslik_3=QtWidgets.QLabel(self)
+        self.baslik_4=QtWidgets.QLabel(self)
+        
+        self.baslik_1.setText("Kayıtlı Not-1")
+        self.baslik_2.setText("Kayıtlı Not-2")
+        self.baslik_3.setText("Kayıtlı Not-3")
+        self.baslik_4.setText("Kayıtlı Not-4")
+        
+        self.baslik_1.move(400,10)
+        self.baslik_2.move(400,115)
+        self.baslik_3.move(400,220)
+        self.baslik_4.move(400,325)
         #---------------------------------------------------------
         
         self.show()
     
         
-    def kaydet_clicked(self):
+    def kaydet_clicked(self):   #Kaydet butonuna tıkladığımızda notu kayıtlı notlara kaydeden fonksiyonun komut satırı
         yazi=self.yazi_alani.toPlainText()
 
         if not self.kayitli_kutucuk.toPlainText():
-            self.kayitli_kutucuk.append(yazi)
-            self.yazi_alani.clear()
-            self.kayitli_kutucuk.setDisabled(True)
+            self.kayitli_kutucuk.append(yazi)        #Yazıyı kayıtlı kutucuklara yerleştiren kod satırı
+            self.yazi_alani.clear()                  #Yazıyı kaydettikten sonra ana kısmın tekrardan boş kalmasını sağlayan kod satırı
+            self.kayitli_kutucuk.setDisabled(True)   #Kayıtlı kutucuğun hiçbir şekilde değiştirilemez olmasını sağlayan kod satırı
         
         elif not self.kayitli_kutucuk_2.toPlainText():
             self.kayitli_kutucuk_2.append(yazi)
@@ -85,8 +96,8 @@ class Pencere(QtWidgets.QWidget):
         else:
             pass    
 
-    #Radio butonlarla beraber tanımlanan click fonksiyonunu kutucukların silimine bağlayan kısım    
-    def temizle_clicked(self):
+       
+    def temizle_clicked(self):     #Radio butonlarla beraber tanımlanan click fonksiyonunu kutucukların silimine bağlayan kısım 
         if self.sec1.isChecked():
             self.kayitli_kutucuk.clear()
         elif self.sec2.isChecked():
@@ -96,7 +107,7 @@ class Pencere(QtWidgets.QWidget):
         elif self.sec4.isChecked():
             self.kayitli_kutucuk_4.clear()
 
-    def ac_clicked(self):
+    def ac_clicked(self):   #Aç butonuna tıkladığımızda notu ana ekrana yazdıran fonksiyonun komut satırı
         if self.sec1.isChecked():
             ac_text = self.kayitli_kutucuk.toPlainText()
             self.yazi_alani.setPlainText(ac_text)
@@ -112,9 +123,7 @@ class Pencere(QtWidgets.QWidget):
             
 
 
- 
-
 
 app=QtWidgets.QApplication(sys.argv)
 pencere=Pencere()
-sys.exit(app.exec_())    
+sys.exit(app.exec_())     
