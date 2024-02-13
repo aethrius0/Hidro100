b9b965b08d32dc2b8753189c81059aa30ae4bda1
selinlin2 <158591749+selinlin2@users.noreply.github.com>
2024-02-13 19:03:48 +0300

    Update speedometer.py

--- speedometer.py
+++ speedometer.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import sys
 import time
 from PyQt5.QtCore import QObject, QUrl, Qt, pyqtProperty, pyqtSignal
@@ -11,7 +10,15 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     view = QQuickView()
     view.setSource(QUrl('speedometer.qml'))
-    gauge=view.findChild(QObject,'test_gauge')
-    gauge.setProperty('gauge_value',0)
+    gauge=view.findChild(QObject,'speed_gauge')
+    gauge2=view.findChild(QObject,'wh_gauge')
+    gauge3=view.findChild(QObject,'temp_gauge')
+    gauge4=view.findChild(QObject,'volt_gauge')
+    gauge5=view.findChild(QObject,'bt_gauge')
+    gauge.setProperty('speedgauge_value',0)
+    gauge2.setProperty('whgauge_value',0)
+    gauge3.setProperty('tempgauge_value',0)
+    gauge4.setProperty('voltgauge_value',0)
+    gauge5.setProperty('btgauge_value',0)
     view.show()
     sys.exit(app.exec_())
