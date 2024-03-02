ee51b163f9d8b26675a0514eee90a662cf36657a
Kemal <Kemalbuyrul@yandex.com>
2024-03-02 13:17:22 +0300

    Drgisiklik yapildi.

--- veri_isleme.py
+++ veri_isleme.py
@@ -1,6 +1,14 @@
-def parse_byte_object(byte_data):
+byte_object = "135168468416165146854986794865"
+
+def Check_data(byte_object):
+    if isinstance(byte_object,bytes):
+        return "ok"
+    else:
+        return "error"    
+
+def parse_by_object(byte_object):
     # Veriyi 4'lü gruplara ayır
-    groups = [byte_data[i:i+4] for i in range(0, len(byte_data), 4)]
+    groups = [byte_object[i:i+4] for i in range(0, len(byte_object), 4)]
 
     # Her bir 4'lü grubun içindeki her ikinci sayıdan sonra virgül ekle
     formatted_groups = []
@@ -9,13 +17,25 @@ def parse_byte_object(byte_data):
         formatted_groups.append(formatted_group)
 
     return formatted_groups
+    
 
-# Kullanıcıdan veri girişi al - terminalden 
-byte_data = input("Lütfen byte verisini girin: ")
+def Getvalues(formatted_data):
+    # Parçalanan veriyi alarak değerleri ayıkla ve adlandır
+    volt = float(formatted_data[0][2:])
+    temperature = float(formatted_data[1][2:])
+    SoC = float(formatted_data[2][:2])
+    wh = float(formatted_data[3][:2])
+    speed = float(formatted_data[4][:2])
+    
+    return volt, temperature, SoC, wh, speed
 
-# Veriyi işle - parse fonksiyonu 
-formatted_data = parse_byte_object(byte_data)
+formatted_data = parse_by_object(byte_object)
 
-# Sonucu yazdırma kısmı 
-for group in formatted_data:
-    print(group)
+
+volt, temperature, SoC, wh, speed = Getvalues(formatted_data)
+
+print("Volt:", volt)
+print("Temperature:", temperature)
+print("SoC:", SoC)
+print("Wh:", wh)
+print("Speed:", speed)
